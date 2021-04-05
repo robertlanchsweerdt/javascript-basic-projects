5c93102bd92870012657b086657cd839a5ef8b2e
robertlanchsweerdt <signaleightdesign@gmail.com>
2021-04-05 16:59:23 -0400

    updated app.js

--- 4-navbar/setup/app.js
+++ 4-navbar/setup/app.js
@@ -3,3 +3,12 @@
 // add - add class
 // remove - remove class
 // toggle - toggles class
+
+const hamburger = document.querySelector('.fa-bars');
+const menu = document.querySelector('.links');
+
+hamburger.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+  menu.classList.toggle('show-links');
+}
