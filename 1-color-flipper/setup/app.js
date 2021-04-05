e29ed54d2ed5aad0c3f3ac1c75df8d835ae348b2
robertlanchsweerdt <signaleightdesign@gmail.com>
2021-04-05 09:29:00 -0400

    updates app.js file

--- 1-color-flipper/setup/app.js
+++ 1-color-flipper/setup/app.js
@@ -1,1 +1,17 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ['green', 'red', 'rgba(133,122,200)', '#f15025'];
+
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+const siteBody = document.querySelector('body');
+
+btn.addEventListener('click', changeColor);
+
+function changeColor() {
+  const randomColor = chooseColor();
+  siteBody.style.backgroundColor = colors[randomColor];
+  color.innerText = colors[randomColor];
+}
+
+function chooseColor() {
+  return Math.floor(Math.random() * colors.length);
+}
