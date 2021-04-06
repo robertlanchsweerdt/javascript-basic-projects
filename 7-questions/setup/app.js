75f3e4c5c2856b4b50cd499215a8c367f4420e6f
robertlanchsweerdt <signaleightdesign@gmail.com>
2021-04-06 16:33:40 -0400

    updated index.html and addeed code to app.js for Questions project

--- 7-questions/setup/app.js
+++ 7-questions/setup/app.js
@@ -1,2 +1,24 @@
 //using selectors inside the element
+let questionArticle = document.querySelectorAll('.question');
+
 // traversing the dom
+questionArticle.forEach((question) => {
+  const questionButton = question.querySelector('.question-btn');
+
+  questionButton.addEventListener('click', function () {
+    closeOpenBoxes(question);
+    operateBox(question);
+  });
+});
+
+function closeOpenBoxes(question) {
+  questionArticle.forEach((foo) => {
+    if (foo !== question) {
+      foo.classList.remove('show-text');
+    }
+  });
+}
+
+function operateBox(question) {
+  question.classList.toggle('show-text');
+}
