ff2ab448a392fdb536289a91efc197768be1e593
robertlanchsweerdt <signaleightdesign@gmail.com>
2021-04-06 08:47:34 -0400

    updated index.html and app.js for Modal project

--- 6-modal/setup/app.js
+++ 6-modal/setup/app.js
@@ -1,4 +1,20 @@
 // select modal-btn,modal-overlay,close-btn
+const modal = document.querySelector('.modal-overlay');
+const btns = document.querySelectorAll('button');
+const btnClose = document.querySelector('.close-btn');
+
 // listen for click events on modal-btn and close-btn
+btnModal.addEventListener('click', openModal);
+
 // when user clicks modal-btn add .open-modal to modal-overlay
+btnClose.addEventListener('click', closeModal);
+
+// when user clicks on btnModal open Modal
+function openModal() {
+  modal.classList.add('open-modal');
+}
+
 // when user clicks close-btn remove .open-modal from modal-overlay
+function closeModal() {
+  modal.classList.remove('open-modal');
+}
